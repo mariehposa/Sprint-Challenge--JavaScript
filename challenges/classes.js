f4462817fc60706dfcbc87db1b165d3a7d9ce362
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2020-03-19 21:52:49 +0100

    .

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,18 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
-
+/*
+function CuboidMaker(length, width, height){
+    this.length = length;
+    this.width = width;
+    this.height = height;
+}
+CuboidMaker.prototype.volume = function(){
+    return this.length * this.width * this.height;
+}
+CuboidMaker.prototype.surfaceArea = function(){
+    return 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
+}
+const cuboid = new CuboidMaker(4, 5, 5)
+*/
 class CuboidMakers {
     constructor(length, width, height) {
         this.length = length;
